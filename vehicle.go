bf096ba4015e30e4baaab93e42748d4b131f4117
Ebunona1 <ebunona1@gmail.com>
2021-09-28 16:37:40 +0100

    completed module 2

--- vehicle.go
+++ vehicle.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
 type vehicle interface {
 }
 
@@ -70,11 +79,11 @@ func init() {
 
 func main() {
 	// Generate ratings for the different vehicles
+	generateRating()
 
 	// Print ratings for the different vehicles
 }
 
-/*
 func readJSONFile() Values {
 	jsonFile, err := os.Open("feedback.json")
 
@@ -90,4 +99,47 @@ func readJSONFile() Values {
 
 	return content
 }
-*/
+
+func generateRating() {
+	f := readJSONFile()
+
+	for _, v := range f.Models {
+		var vehResult feedbackResult
+		var vehRating rating
+
+		for _, msg := range v.Feedback {
+			if text := strings.Split(msg, " "); len(text) >= 5 {
+				vehRating = 5.0
+				vehResult.feedbackTotal++
+
+				for _, word := range text {
+					// fmt.Println(strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"))
+					switch s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"); s {
+					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
+						vehRating += extraPositive
+						fmt.Printf("Case 1: %.2f\n", vehRating)
+					case "help", "helpful", "thanks", "thank you", "happy":
+						vehRating += positive
+						fmt.Printf("Case 2: %.2f\n", vehRating)
+					case "not helpful", "sad", "angry", "improve", "annoy":
+						vehRating += negative
+						fmt.Printf("Case 3: %.2f\n", vehRating)
+					case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
+						vehRating += extraNegative
+						fmt.Printf("Case 4: %.2f\n", vehRating)
+					}
+				}
+
+				switch {
+				case vehRating > 8.0:
+					vehResult.feedbackPositive++
+				case vehRating >= 4.0 && vehRating <= 8.0:
+					vehResult.feedbackNeutral++
+				case vehRating < 4.0:
+					vehResult.feedbackNegative++
+				}
+			}
+		}
+		vehicleResult[v.Name] = vehResult
+	}
+}
